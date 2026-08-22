3e9551e98973198c3bb1799f6c86f4cd04657077
one-loop <ss17886@nyu.edu>
2026-08-22 20:42:35 +0530

    Fix ExperienceProvider context issue

--- src/components/ClientLayout.js
+++ src/components/ClientLayout.js
@@ -2,13 +2,17 @@
 import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 
+import { ExperienceProvider } from '../context/ExperienceContext';
+
 export default function ClientLayout({ children }) {
   const [isReady, setIsReady] = useState(false);
   useEffect(() => { setIsReady(true); }, []);
   return (
-    <div className="app">
-      <Navbar />
-      {children}
-    </div>
+    <ExperienceProvider>
+      <div className="app">
+        <Navbar />
+        {children}
+      </div>
+    </ExperienceProvider>
   );
 }
