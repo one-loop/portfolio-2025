e6583a18d2d60b360610037d6e2be31525caf843
one-loop <ss17886@nyu.edu>
2026-08-23 21:17:05 +0530

    Fix double-animation bug by migrating to Next.js template.js

--- src/components/ClientLayout.js
+++ src/components/ClientLayout.js
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from 'react';
 import Navbar from './Navbar';
 
 import { ExperienceProvider } from '../context/ExperienceContext';
-import PageTransition from './PageTransition';
 
 export default function ClientLayout({ children }) {
   const [isReady, setIsReady] = useState(false);
@@ -12,9 +11,7 @@ export default function ClientLayout({ children }) {
     <ExperienceProvider>
       <div className="app">
         <Navbar />
-        <PageTransition>
-          {children}
-        </PageTransition>
+        {children}
       </div>
     </ExperienceProvider>
   );
